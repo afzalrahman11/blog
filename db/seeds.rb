d050c800e727f40794bdb512542adf35b28e3a96
Afzal Rahman <afzalrahman123@gmail.com>
2025-02-20 21:49:53 +0530

    Add devise gem for User authentication

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+user = User.find_or_initialize_by(email: "afzal@gmail.com")
+user.update!(
+  password: "password",
+  password_confirmation: "password"
+)
