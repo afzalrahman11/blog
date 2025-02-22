30913b8bbdc433373774b964e24b73882cd991f7
Afzal Rahman <afzalrahman123@gmail.com>
2025-02-22 10:22:23 +0530

    Add style from tailwind css to the pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable, :registerable(allow users to sign up),
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
 end
